32a40fedddc81da3dd90540c72c156c68d25b0d6
Bharamagouda <Bharamagouda@LAPTOP-38121HEH>
2020-12-16 17:27:09 +0530

    change1

--- JpaProject/src/main/java/com/middleware/demo/JpaProjectApplication.java
+++ JpaProject/src/main/java/com/middleware/demo/JpaProjectApplication.java
@@ -8,6 +8,7 @@ public class JpaProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaProjectApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
